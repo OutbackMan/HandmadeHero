a66dc5c92e76092bab1df0aad88b6c4285aa5885
Russell Coight <rj.coight@gmail.com>
2019-02-07 11:47:47 +1100

    more

--- windows-build.bat
+++ windows-build.bat
@@ -1,19 +1,19 @@
 @ECHO OFF
 
-REM NOTE(Ryan): As of January 2019, unable to use lld with clang/mingw. 
-REM Therefore, resort to clang/msvc, i.e. visual studio c++ build tools and windows 10 SDK
+REM jre+cdt(rename zip before extraction due to long java naming conventions), vs c++ build tools + win10 sdk, llvm, gdb (mingw-64 sourceforge builds)
+
+REM NOTE(Ryan): Unable to use clang + lld + mingw due to bug documented here: https://bugs.llvm.org/show_bug.cgi?id=38939  
+REM Therefore, resort to using slower ld from binutils.
 
 SET common_compiler_flags=-Wall -Wextra -Wpedantic -Wfloat-equal -Wunreachable-code -Wshadow^
- -fuse-ld=lld-link.exe -luser32 -lgdi32 -lxinput -ldsound
+ -luser32 -lgdi32 -lxinput -ldsound
 
-SET debug_compiler_flags=-fno-omit-frame-pointer -fno-optimize-sibling-calls^
- -g -gcodeview -Wl,/debug,/pdb:windows-hh.pdb
+SET debug_compiler_flags=-fno-omit-frame-pointer -fno-optimize-sibling-calls -g
 
 SET release_compiler_flags=-Ofast
 
 IF NOT EXIST build MKDIR build
 PUSHD build
-DEL hh.pdb > NUL 2> NUL
 
 clang.exe %common_compiler_flags% %debug_compiler_flags% ..\code\windows-hh.c -o windows-hh.exe
 
